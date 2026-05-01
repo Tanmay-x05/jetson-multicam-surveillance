1842274d0c42dc6a2c8260db8667420d4d5b34a9
Tanmay-x05 <tanmay.s1910@gmail.com>
2026-05-01 23:50:05 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+CAMERA_SOURCES = [0 ,"data/cam1.mp4", "data/cam2.mp4", 1]
+CAMERA_LABELS = ["Camera 1", "Camera 2", "Camera 3", "Camera 4"]
+
+FRAME_WIDTH = 640
+FRAME_HEIGHT = 480
+FPS = 20
+
+DETECT_CLASSES = [1]
+CONFIDENCE_THRESH = 0.5
+
+LIVE_GRID_COLS = 2
+
+RECORD_COLS = None
+RECORD_ROWS = None
+
+ALERT_COOLDOWN_SECONDS = 30
+
+SNAPSHOT_DIR = "data/snapshots"
+RECORDING_DIR = "data/recordings"
+
+LOG_FILE = "surveillance.log"
